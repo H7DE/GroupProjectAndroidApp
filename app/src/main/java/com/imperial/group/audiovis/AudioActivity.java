bf598a0225eafe5f4084c8e89f5a5e0419df3966
rick <ricardobusy@live.co.uk>
2015-10-19 21:26:04 +0100

    Added recording permission

--- app/src/main/java/com/imperial/group/audiovis/AudioActivity.java
+++ app/src/main/java/com/imperial/group/audiovis/AudioActivity.java
@@ -17,7 +17,7 @@ import android.widget.TextView;
 public class AudioActivity extends Activity {
 
     public static final String DEBUG = "DEBUG";
-    private AudioRecord ar;
+    private AudioRecord recorder;
     private static final int RECORDER_SAMPLERATE = 8000;
     private static final int RECORDER_CHANNELS = AudioFormat.CHANNEL_IN_MONO;
     private static final int RECORDER_AUDIO_ENCODING = AudioFormat.ENCODING_PCM_16BIT;
@@ -29,12 +29,12 @@ public class AudioActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.audio_layout);
-//        ar = new AudioRecord(MediaRecorder.AudioSource.MIC, 44100,
+//        recorder = new AudioRecord(MediaRecorder.AudioSource.MIC, 44100,
 //                RECORDER_SAMPLERATE, RECORDER_AUDIO_ENCODING, AudioRecord.getMinBufferSize(RECORDER_SAMPLERATE, RECORDER_CHANNELS, RECORDER_AUDIO_ENCODING));
 
         TextView msg = (TextView) findViewById(R.id.textView2);
         msg.setText("Foo");
-//        Log.d("DEBUG", "" + ar.getState());
+//        Log.d("DEBUG", "" + recorder.getState());
 
         //CONFIGURE BUTTONS
         //START/STOP recording
@@ -51,11 +51,47 @@ public class AudioActivity extends Activity {
             case R.id.recordingCheckBox:
                 if (checked) {
                     Log.d(DEBUG, "TRUE");
+                    startRecording();
                 } else {
+                    stopRecording();
                     Log.d(DEBUG, "FALSE");
                 }
                 break;
         }
     }
 
+    private void startRecording() {
+        int BufferElements2Rec = 1024; // want to play 2048 (2K) since 2 bytes we use only 1024
+        int BytesPerElement = 2; // 2 bytes in 16bit format
+        recorder = new AudioRecord(MediaRecorder.AudioSource.MIC,
+                RECORDER_SAMPLERATE, RECORDER_CHANNELS,
+                RECORDER_AUDIO_ENCODING, BufferElements2Rec * BytesPerElement);
+
+        recorder.startRecording();
+        isRecording = true;
+        recordingThread = new Thread(new Runnable() {
+            public void run() {
+                Log.d(DEBUG, "RECORDING");
+            }
+        }, "AudioRecorder Thread");
+        recordingThread.start();
+    }
+
+    private void stopRecording() {
+        // stops the recording activity
+        if (null != recorder) {
+            isRecording = false;
+            recorder.stop();
+            recorder.release();
+            recorder = null;
+            recordingThread = null;
+        }
+    }
+
+
+    @Override
+    protected void onPause() {
+        stopRecording();
+        super.onPause();
+    }
 }
